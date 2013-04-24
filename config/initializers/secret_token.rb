9b60aaebab87d66be6782668c7246a102f35b03f
toshiotm <toshi.otm@gmail.com>
2013-04-24 23:42:51 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eathub::Application.config.secret_key_base = 'a4afb3992f618eded6c4d6796ec58f03b4322e143b76d2729116db84f4193f1e4c4275dc0084a5427fb81e7a07f8ac1f34b3a6148f1ee734d8a81355d9cc251a'
